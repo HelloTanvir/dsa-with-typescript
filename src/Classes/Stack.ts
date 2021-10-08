8631e01070f9834c8b6f79ab3a01dd15bf9532fa
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2021-10-08 12:13:50 +0600

    made a stack

---
+++ src/Classes/Stack.ts
@@ -0,0 +1,35 @@
+export default class Stack {
+  private storage: { [key: number]: any };
+  private count: number;
+
+  constructor() {
+    this.storage = {};
+    this.count = 0;
+  }
+
+  // adds a value at the end of the stack
+  push(value: any) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+
+  // removes a value from the end of the stack
+  pop() {
+    if (this.count === 0) return undefined;
+
+    this.count--;
+    const removedValue = this.storage[this.count];
+    delete this.storage[this.count];
+    return removedValue;
+  }
+
+  // show the value of the end of the stack'
+  peek() {
+    return this.storage[this.count - 1];
+  }
+
+  // show the length of the stack
+  length() {
+    return this.count;
+  }
+}
